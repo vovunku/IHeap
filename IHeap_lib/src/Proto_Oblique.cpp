3c0dfe121e7d8e2ba864c103cafe651e220658b8
vovunku <33938211+vovunku@users.noreply.github.com>
2020-02-04 19:03:57 +0300

    updated

---
+++ IHeap_lib/src/Proto_Oblique.cpp
@@ -0,0 +1,79 @@
+#include <Proto_Oblique.h>
+
+#include<iostream>
+#include <exception>
+
+Proto_Oblique::Oblique_Node::Oblique_Node(int value){
+    key = value;
+    left_child = nullptr;
+    right_child = nullptr;
+}
+
+Proto_Oblique::Proto_Oblique(){
+    _head = nullptr;
+}
+/*
+Proto_Oblique::Proto_Oblique(int value){
+    _head = new Oblique_Node(value);
+}*/
+
+void Proto_Oblique::_delete_tree(Oblique_Node* node){
+    if(node != nullptr){
+        _delete_tree(node->left_child);
+        _delete_tree(node->right_child);
+        delete node;
+    }
+}
+
+Proto_Oblique::~Proto_Oblique(){
+    _delete_tree(_head);
+}
+
+Proto_Oblique::Oblique_Node* Proto_Oblique::Oblique_Node::meld(Oblique_Node* right){
+    if(right == nullptr){
+        return this;
+    }else if(this == nullptr){
+        return right;
+    }
+    Oblique_Node *left = this;
+    if(left->key > right->key){
+        std::swap(left, right);
+    }
+    left->right_child = left->right_child->meld(right);
+    left->invariant();
+    return left;
+}
+
+void Proto_Oblique::meld(Heap_Interface& other){
+    Proto_Oblique& other_Skew = dynamic_cast<Proto_Oblique&> (other);
+    if(other_Skew._head == nullptr){
+        return;
+    }else if(this->_head == nullptr){
+        this->_head = other_Skew._head;
+        other_Skew._head = nullptr;
+        return;
+    }
+    this->_head = this->_head->meld(other_Skew._head);
+    other_Skew._head = nullptr;
+}
+
+/*void Proto_Oblique::insert(int value) {
+    Proto_Oblique other(value);
+    this->meld(other);
+}*/
+
+int Proto_Oblique::get_min() const{
+    if(this->_head == nullptr){
+        throw std::logic_error("nothing to get");
+    }
+    return this->_head->key;
+}
+
+void Proto_Oblique::extract_min() {
+    if(this->_head == nullptr){
+        throw std::logic_error("nothing to extract");
+    }
+    Oblique_Node* min_element = this->_head;
+    this->_head = min_element->left_child->meld(min_element->right_child);
+    delete min_element;
+}
--- IHeap_lib/src/Skew_Heap.cpp
+++ IHeap_lib/src/Skew_Heap.cpp
@@ -3,77 +3,20 @@
 #include<iostream>
 #include <exception>
 
-Skew_Heap::Skew_Node::Skew_Node(int value){
-    key = value;
-    left_child = nullptr;
-    right_child = nullptr;
-}
-
-Skew_Heap::Skew_Heap(){
-    _head = nullptr;
-}
-
 Skew_Heap::Skew_Heap(int value){
-    _head = new Skew_Node(value);
+    _head = create_node(value);
 }
 
-void Skew_Heap::_delete_tree(Skew_Node* node){
-    if(node != nullptr){
-        _delete_tree(node->left_child);
-        _delete_tree(node->right_child);
-        delete node;
-    }
+Skew_Heap::Skew_Node * Skew_Heap::create_node(int value) {
+    Skew_Node* result = new Skew_Node(value);
+    return result;
 }
 
-Skew_Heap::~Skew_Heap(){
-    _delete_tree(_head);
-}
-
-Skew_Heap::Skew_Node* Skew_Heap::Skew_Node::meld(Skew_Node* right){
-    if(right == nullptr){
-        return this;
-    }else if(this == nullptr){
-        return right;
-    }
-    Skew_Node *left = this;
-    if(left->key > right->key){
-        std::swap(left, right);
-    }
-    left->right_child = left->right_child->meld(right);
-    std::swap(left->right_child, left->left_child);
-    return left;
-}
-
-void Skew_Heap::meld(Heap_Interface& other){
-    Skew_Heap& other_Skew = dynamic_cast<Skew_Heap&> (other);
-    if(other_Skew._head == nullptr){
-        return;
-    }else if(this->_head == nullptr){
-        this->_head = other_Skew._head;
-        other_Skew._head = nullptr;
-        return;
-    }
-    this->_head = this->_head->meld(other_Skew._head);
-    other_Skew._head = nullptr;
-}
-
-void Skew_Heap::insert(int value) {
+void Skew_Heap::insert(int value){
     Skew_Heap other(value);
     this->meld(other);
 }
 
-int Skew_Heap::get_min() const{
-    if(this->_head == nullptr){
-        throw std::logic_error("nothing to get");
-    }
-    return this->_head->key;
-}
-
-void Skew_Heap::extract_min() {
-    if(this->_head == nullptr){
-        throw std::logic_error("nothing to extract");
-    }
-    Skew_Node* min_element = this->_head;
-    this->_head = min_element->left_child->meld(min_element->right_child);
-    delete min_element;
+void Skew_Heap::Skew_Node::invariant() {
+    std::swap(this->right_child, this->left_child);
 }
